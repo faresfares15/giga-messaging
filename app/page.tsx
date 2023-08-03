d3961c6743a31b93238a49c431d4dabd3d8ebcb5
faresfares15 <lf_aoufar@esi.dz>
2023-08-03 15:36:17 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex flex-1 items-center justify-center h-screen w-screen">
+      <p className="font-black text-xl">Hello Kassoum</p>
+    </main>
+  )
+}
